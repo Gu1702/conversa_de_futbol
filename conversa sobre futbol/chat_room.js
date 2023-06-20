b937aef0c679d2bf39375687dfbf671ed0f42b05
Gu1702 <135768371+Gu1702@users.noreply.github.com>
2023-06-20 18:58:49 -0300

    Update chat_room.js
    comecei o chat_room.js.

---
+++ conversa sobre futbol/chat_room.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDseGWB0urmV9e07MKpn1JvEg90N3_xLvA",
+    authDomain: "app-de-conversa-byjus.firebaseapp.com",
+    databaseURL: "https://app-de-conversa-byjus-default-rtdb.firebaseio.com",
+    projectId: "app-de-conversa-byjus",
+    storageBucket: "app-de-conversa-byjus.appspot.com",
+    messagingSenderId: "54217362825",
+    appId: "1:54217362825:web:9032bf1a721fc25254e558"
+  };
